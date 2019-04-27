6d83608292cf59991d52a0a9db06eb85ae00d180
Saheel Wagh <saheelwagh@Gmail.com>
2019-04-27 21:53:28 +0530

    set up files

--- ethreum-petshop/src/js/app.js
+++ ethreum-petshop/src/js/app.js
@@ -24,76 +24,78 @@ App = {
     },
 
     initWeb3: async function () {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        App.web3Provider = window.ethereum;
-        try {
-          // Request account access
-          await window.ethereum.enable();
-        } catch (error) {
-          // User denied account access...
-          console.error("User denied account access")
+        // Modern dapp browsers...
+        if (window.ethereum) {
+          App.web3Provider = window.ethereum;
+          try {
+            // Request account access
+            await window.ethereum.enable();
+          } catch (error) {
+            // User denied account access...
+            console.error("User denied account access")
+          }
         }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        App.web3Provider = window.web3.currentProvider;
-      }
-      // If no injected web3 instance is detected, fall back to Ganache
-      else {
-        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-      }
-      web3 = new Web3(App.web3Provider
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          App.web3Provider = window.web3.currentProvider;
+        }
+        // If no injected web3 instance is detected, fall back to Ganache
+        else {
+          App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        }
+        web3 = new Web3(App.web3Provider
 
-          return App.initContract();
-        },
+            return App.initContract();
+          },
 
-        initContract: function () {
-          $.getJSON('Adoption.json', data => {
-            //get necessary artifact and initiate it with truffle-contract
-            const AdoptionArtifact = data;
-            App.contracts.Adoption = TruffleContract(AdoptionArtifact)
-          });
-          ///set provider for our contract
-          App.contracts.Adoption.setProvider(App.web3Provider);
-          //use our contract to retrieve and mark the adopted pets
-          return App.markAdopted()
-          return App.bindEvents();
-        },
+          initContract: function () {
+            $.getJSON('Adoption.json', data => {
+              //get necessary artifact and initiate it with truffle-contract
+              const AdoptionArtifact = data;
+              App.contracts.Adoption = TruffleContract(AdoptionArtifact)
+            });
+            ///set provider for our contract
+            App.contracts.Adoption.setProvider(App.web3Provider);
+            //use our contract to retrieve and mark the adopted pets
+            return App.markAdopted()
+            return App.bindEvents();
+          },
 
-        bindEvents: function () {
-          $(document).on('click', '.btn-adopt', App.handleAdopt);
-        },
+          bindEvents: function () {
+            $(document).on('click', '.btn-adopt', App.handleAdopt);
+          },
 
-        markAdopted: function (adopters, account) {
-          const adoptionInstance;
-          App.contracts.Adoption.deployed().then(instance => {
-            adoptionInstance = instance
-            return adoptionInstance.getAdopters.call()
-          }).then(adopters => {
-            for (let i = 0; i < adopters.length; i++) {
-              if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
-                $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+          markAdopted: function (adopters, account) {
+            const adoptionInstance;
+            App.contracts.Adoption.deployed().then(instance => {
+              adoptionInstance = instance
+              return adoptionInstance.getAdopters.call()
+            }).then(adopters => {
+              for (let i = 0; i < adopters.length; i++) {
+                if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+                  $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
 
+                }
+              }
+            }).catch(error => console.log(error.message))
+          },
+
+          handleAdopt: function (event) {
+            event.preventDefault();
+
+            var petId = parseInt($(event.target).data('id'));
+
+            const adoptionInstance;
+            web3.eth.getAccounts((error, Accounts) => {
+                if (error) {
+                  console.log(error);
+                }
               }
             }
-          }).catch(error => console.log(error.message))
-        },
+          };
 
-        handleAdopt: function (event) {
-          event.preventDefault();
-
-          var petId = parseInt($(event.target).data('id'));
-
-          /*
-           * Replace me...
-           */
-        }
-
-    };
-
-    $(function () {
-      $(window).load(function () {
-        App.init();
-      });
-    });
+        $(function () {
+          $(window).load(function () {
+            App.init();
+          });
+        });
